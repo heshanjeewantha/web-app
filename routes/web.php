9811a0d11e3348649002d0b338478682acdb9e54
it22578532 <heshanjeewantha@gmail.com>
2025-03-28 20:07:57 +0530

    service settings manage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\SettingsController;
 use App\Http\Controllers\admin\TestimonialController;
 use App\Http\Controllers\admin\SliderController;
 use App\Http\Controllers\ProfileController;
@@ -39,4 +40,10 @@ Route::controller(TestimonialController::class)->middleware(['auth','verified'])
     Route::post('/TestimonialUpdate','updateTestimonial')->name('Testimonial.update');
     Route::get('/deleteTestimonial/{id}','deleteTestimonial')->name('Testimonial.delete');
 });
+
+Route::controller(SettingsController::class)->middleware(['auth','verified'])->group(function (){
+    Route::get('/settings','index')->name('settings'); 
+    Route::post('/settingUpdate','update')->name('settings.update');
+ });
+ 
 require __DIR__.'/auth.php';
