0c22dae07d271bd3dc18ec00dd99e78cd486a8fb
it22578532 <heshanjeewantha@gmail.com>
2025-03-28 23:06:35 +0530

    blog section complete

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,61 @@
 <?php
 
+use App\Http\Controllers\admin\PermissionController;
+use App\Http\Controllers\admin\PostsController;
+use App\Http\Controllers\admin\RoleController;
 use App\Http\Controllers\admin\SettingsController;
-use App\Http\Controllers\admin\TestimonialController;
 use App\Http\Controllers\admin\SliderController;
+use App\Http\Controllers\admin\TestimonialController;
+use App\Http\Controllers\admin\UserController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\TimeRestrictedAccess;
+use App\Models\Posts;
 use App\Models\Slider;
 use App\Models\Testimonial;
 use Illuminate\Support\Facades\Route;
-
+use Ramsey\Uuid\Type\Time;
 
 Route::get('/', function () {
-    $sliders=Slider::all();
-    $testimonials=Testimonial::all();
-    return view('frontend.home',compact('sliders','testimonials'));	
+
+    $sliders = Slider::all();
+    $testimonials = Testimonial::all();
+    return view('frontend.home',compact('sliders','testimonials'));
+});
+
+Route::get('/about', function () {
+    return view('frontend.about');
 });
 
 
+Route::get('/service', function () {
+    return view('frontend.service');
+})->middleware([TimeRestrictedAccess::class]);
 
+
+Route::get('/blog', function () {
+    $posts = Posts::orderBy('created_at','desc')->paginate(6);
+
+    return view('frontend.blog',compact('posts'));
+});
+
+Route::get('/contact', function () {
+    return view('frontend.contact');
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    $post = Posts::where('slug', $slug)->first();
+    return view('frontend.post-single',compact('post'));
+});
 Route::get('/dashboard', function () {
-    return view('admin.dashbord');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(ContactController::class)->group(function () {
+    Route::get('/adminContact', 'index')->middleware(['role:super-admin'])->name('contact');
+    Route::post('/contactSave', 'store')->name('contact.store');    
+    Route::get('/contact/{id}/delete', 'destroy')->middleware(['role:super-admin'])->name('contact.delete');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -27,7 +63,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::controller(SliderController::class)->middleware(['auth', 'verified'])->group(function () {
+Route::controller(SliderController::class)->middleware(['auth','verified'])->group(function (){
     Route::get('/SliderIndex','Index')->name('slider.index');
     Route::post('/saveSlider','storeslider')->name('slider.store');
     Route::post('/sliderUpdate','updateslider')->name('slider.update');
@@ -41,9 +77,47 @@ Route::controller(TestimonialController::class)->middleware(['auth','verified'])
     Route::get('/deleteTestimonial/{id}','deleteTestimonial')->name('Testimonial.delete');
 });
 
+
 Route::controller(SettingsController::class)->middleware(['auth','verified'])->group(function (){
-    Route::get('/settings','index')->name('settings'); 
-    Route::post('/settingUpdate','update')->name('settings.update');
+   Route::get('/settings','index')->name('settings'); 
+   Route::post('/settingUpdate','update')->name('settings.update');
+});
+
+Route::controller(PostsController::class)->middleware(['auth','verified'])->group(function (){
+   
+    Route::get('/postIndex','index');
+    Route::post('/savePost','storepost')->name('post.store');
+    Route::post('/postUpdate','updatepost')->name('post.update');
+    Route::get('/deletePost/{id}','deletepost')->name('post.delete');
+   
+ });
+
+ Route::controller(PermissionController::class)->middleware(['auth','verified'])->group(function (){
+    
+    Route::get('/permissionIndex','index');
+    Route::post('/savePermission','storepermission')->name('permission.store');
+    Route::post('/permissionUpdate','updatepermission')->name('permission.update');
+    Route::get('/deletePermission/{id}','deletepermission')->middleware(['role:super-admin'])->name('permission.delete');
+ });
+
+ Route::controller(RoleController::class)->middleware(['auth','verified'])->group(function (){
+    
+    Route::get('/roleIndex','index');
+    Route::post('/saveRole','storerole')->name('role.store');
+    Route::post('/roleUpdate','updaterole')->name('role.update');
+    Route::get('/deleteRole/{id}','deleterole')->middleware(['role:super-admin'])->name('role.delete');
+
+    Route::get('/permissionToRole/{id}','givePermissionToRole')->name('role.givePermissionToRole');
+    Route::put('/givePermissionToRole/{id}','giveRoleToPermission')->name('role.giveRoleToPermission');
  });
  
+ Route::controller(UserController::class)->middleware(['auth','verified','role:super-admin|admin'])->group(function (){
+    
+    Route::get('/userIndex','index');
+    Route::post('/saveUser','storeuser')->name('user.store');
+    Route::post('/userUpdate','updateuser')->name('user.update');
+    Route::get('/deleteUser/{id}','deleteuser')->middleware(['role:super-admin'])->name('user.delete');
+ });
+ 
+
 require __DIR__.'/auth.php';
